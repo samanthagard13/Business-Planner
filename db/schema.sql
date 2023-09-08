9ca34803970cf75f1742e8882809ee440dcfae23
samantha gard <samantha.gard13@gmail.com>
2023-09-07 20:33:41 -0400

    added schema file with starter employee table

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE business_db;
+
+USE business_db;
+
+CREATE TABLE employees (
+    firstname,
+    lastname,
+    department, 
+    role,
+    manager,
+);
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const inquirer = require("inquirer");
-const Actions = ["Add Employee", "Add Department", "Add Role", "Update Empoyee Role"];
+const express = require('express');
+const mysql = require('mysql');
+
+const Actions = [
+  "Add Employee",
+  "Add Department",
+  "Add Role",
+  "Update Empoyee Role",
+];
 
 const init = () => {
   inquirer
-    .createPromptModule([
+    .prompt([
       {
         type: "list",
         name: "start",
@@ -12,18 +20,48 @@ const init = () => {
       },
     ])
     .then((choices) => {
-        if(choices === Actions[0]) {
-            AddEmployee();
-        }
-        if(choices === Actions[1]) {
-            AddDepartment();
-        }
-        if(choices === Actions[2]) {
-            AddRole();
-        }
-        else if (choices === Actions[3]) {
-            UpdateEmployee();
-        }
+      if (choices === Actions[0]) {
+        AddEmployee();
+      }
+      if (choices === Actions[1]) {
+        AddDepartment();
+      }
+      if (choices === Actions[2]) {
+        AddRole();
+      } else if (choices === Actions[3]) {
+        UpdateEmployee();
+      }
+    });
+};
+
+const AddEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "What is the employee's first name?",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "What is the employee's last name?",
+      },
+      {
+        type: "input",
+        name: "role",
+        message: "What is the employee's role?",
+      },
+      {
+        type: "list",
+        name: "manager",
+        message: "Who is the employee's manager",
+        choices: `{{{managers}}}`,
+      },
+    ])
+    .then((answwers) => {
+      //send to mysql database
+      console.log(`Added ${firstName} ${lastNamer} to the database.`);
     });
 };
 
